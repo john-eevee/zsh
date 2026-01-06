4a91e6b44efcb1a23f546a2d318fa1ad3704c14f
John <5256711+john-eevee@users.noreply.github.com>
2026-01-06 15:43:55 -0300

    feat: add resolution helpers

---
+++ functions/resolutions.zsh
@@ -0,0 +1,9 @@
+function res2k {
+  niri msg output "DP-2" mode "2560x1440"
+  niri msg output "DP-2" scale "1"
+}
+
+function res4k {
+  niri msg output "DP-2" mode "3840x2160"
+  niri msg output "DP-2" scale "1.5"
+}
